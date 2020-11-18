b21e1f80712c147331b7008a7b8b4fb1759f45a6
Radi Cho <radicho123@gmail.com>
2020-11-18 19:25:59 +0200

    Falling squares.

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-var name = prompt("What's your name?");
+// var name = prompt("What's your name?");
+var name = "Radi";
 var element1 = document.getElementById("playerDetails");
 element1.innerHTML = "Your name is: <b>" + name + "</b>";
 
 var element2 = document.getElementById("score");
 // element2.style.background = "yellow";
 
-var sq1 = document.getElementById("sq1");
-var topSize = 0;
+var sqHeight = 50;
+var topSize = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var speeds = [10, 20, 50, 15, 30, 10, 40, 60, 5, 20];
 
-setInterval(function () {
-    sq1.style.top = topSize + "px";
-    topSize += 10;
-}, 100);
+function start(currentSq, i) {
+  setInterval(function() {
+    if (topSize[i] < window.innerHeight - sqHeight) {
+      currentSq.style.top = topSize[i] + "px";
+      topSize[i] += speeds[i];
+    } else {
+      topSize[i] = 0;
+    }
+  }, 100);
+}
+
+for (var i = 0; i < 10; i++) {
+  var currentSq = document.getElementById(`sq${i + 1}`);
+  start(currentSq, i);
+}
