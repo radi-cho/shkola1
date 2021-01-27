1c6cd3056c18418f27b2fef1303ad7299bd73f43
Radi Cho <radicho123@gmail.com>
2021-01-27 14:19:50 +0200

    Merge the two files.

--- events.js
+++ events.js
@@ -1,4 +1,4 @@
-var obj = document.getElementById("obj");
+var obj = document.getElementById("player");
 
 var x = 0;
 var y = 0;
@@ -8,24 +8,23 @@ function moveObj(event) {
     switch(event.keyCode) {
         case 37:
             x -= size;
-            obj.style.left = x;
+            obj.style.left = x + "px";
             break;
         case 38:
             y -= size;
-            obj.style.top = y;
+            obj.style.top = y + "px";
             break;
         case 39:
             x += size;
-            obj.style.left = x;
+            obj.style.left = x + "px";
             break;
         case 40:
             y += size;
-            obj.style.top = y;
+            obj.style.top = y + "px";
             break;
         default:
             // Default
     }
-    console.log(event.keyCode);
 }
 
 document.addEventListener('keydown', moveObj);
